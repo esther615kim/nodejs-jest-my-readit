5ef7ef7d24d4731b90f58b5f58c8d66480dedea0
esther615kim <vilchen@naver.com>
2022-02-21 00:53:49 +0900

    refactor: fetchArticles to get sort_by, order,topic queries

--- models/articles.models.js
+++ models/articles.models.js
@@ -1,119 +1,109 @@
 const db = require("../db/connection");
 
-exports.fetchArticles = ({ sort_by = "created_at", order="desc", topic}) => {
-  const orderedBy = order.toUpperCase();
+exports.fetchArticles = ({ sort_by = "created_at", order = "desc", topic }) => {
 
-  if(topic){ 
-    return db.query(`SELECT * FROM articles WHERE topic in ($1) ORDER BY created_at DESC;`,[topic])
-  .then(({rows}) => {
-    console.log("몇개",rows.length)
-    return rows;
-  })
-}
-  else if(topic&& sort_by){
-    console.log("sort_by",sort_by);
-    return db.query(`SELECT * FROM articles WHERE topic in ($1) ORDER BY $2 DESC;`,[topic,sort_by])
-    .then(({rows}) => {
-      console.log("몇개",rows.length)
-      return rows;
-    })
-  }
-  else{
+  if (topic) {
+    return db
+      .query(
+        `SELECT * FROM articles WHERE topic in ($1) ORDER BY created_at ${order};`,
+        [topic]
+      )
+      .then(({ rows }) => {
+        return rows;
+      });
+  } else if (topic && sort_by) {
+    return db
+      .query(
+        `SELECT * FROM articles WHERE topic in ($1) ORDER BY ${sort_by} ${order};`,
+        [topic]
+      )
+      .then(({ rows }) => {
+        return rows;
+      });
+    }else if(sort_by){
+      return db
+      .query(
+        `SELECT * FROM articles  ORDER BY ${sort_by} ${order};`
+      )
+      .then(({ rows }) => {
+        return rows;
+      });
+  } else {
     // default
     return db
-    .query(`SELECT * FROM articles ORDER BY created_at DESC;`)
-    .then(({rows}) => {
-      console.log("몇개",rows.length)
-      return rows;
-    })
-
+      .query(`SELECT * FROM articles ORDER BY created_at ${order};`)
+      .then(({ rows }) => {
+        return rows;
+      });
   }
-  //   return db
-  //   // .query(`SELECT * FROM articles WHERE topic in ($1) ORDER BY $2 ${order.toUpperCase()};`,[topic,sort_by])
-  //   .query(`SELECT * FROM articles WHERE topic in ($1) ORDER BY $2 DESC;`,[topic,sort_by])
-  //   .then(({rows}) => rows)
-  // }else{
-    return db
-    .query(`SELECT * FROM articles WHERE topic in ($1) ORDER BY created_at DESC;`,[topic])
-    .then(({rows}) => {
-      return rows;
-    })
-  // } 
 };
 
 exports.fetchArticleById = async (id) => {
-
-  const res = await db
-  .query(
+  const res = await db.query(
     "select EXISTS (select * from articles where article_id=$1 limit 1) as success;",
     [id]
   );
 
-  if(res.rows[0].success){
-    const nextRes = await db
-    .query(
+  if (res.rows[0].success) {
+    const nextRes = await db.query(
       `SELECT articles.*, COUNT(comments.article_id) 
     AS comment_count FROM articles 
     LEFT JOIN comments ON articles.article_id = comments.article_id
    WHERE articles.article_id = $1 GROUP BY articles.article_id`,
       [id]
     );
-    console.log(nextRes.rows[0])
+    console.log(nextRes.rows[0]);
     return nextRes.rows[0];
   }
-  console.log("article_id not valid")
+  console.log("article_id not valid");
 };
 
-
-
-
-exports.updateArticle = async(id, update) => {
-  
+exports.updateArticle = async (id, update) => {
   console.log("article:", id, "votes:", update);
 
-   const response = await db
-    .query("UPDATE articles SET votes=votes+$1 WHERE article_id=$2 RETURNING *;", [
-      update,
-      id,
-    ]);
+  const response = await db.query(
+    "UPDATE articles SET votes=votes+$1 WHERE article_id=$2 RETURNING *;",
+    [update, id]
+  );
 
-    return response.rows[0];
+  return response.rows[0];
 };
 
-
 // working but why?
-exports.removeArticle = async(id) => {
+exports.removeArticle = async (id) => {
   console.log("article to delete:", id);
 
-  const res = await db.query("select * from information_schema.table_constraints where table_name = 'comments';");
+  const res = await db.query(
+    "select * from information_schema.table_constraints where table_name = 'comments';"
+  );
 
-  const nextRes = await db.query("DELETE FROM articles where article_id=$1;", [id]);
+  const nextRes = await db.query("DELETE FROM articles where article_id=$1;", [
+    id,
+  ]);
   return nextRes.rowCount;
 };
 
+exports.addArticle = async (newArticle) => {
+  //   const { author, title, body, topic } = newArticle;
+  //   console.log(author,title,body,topic);
 
-exports.addArticle = async(newArticle) => {
+  //   const res = await db
+  //   .query(
+  //     "select EXISTS (select * from users where username=$1 limit 1) as success;",
+  //     [author]
+  //   );
 
-//   const { author, title, body, topic } = newArticle;
-//   console.log(author,title,body,topic);
+  //   if(res.rows){
+  //     const nextRes = await db
+  //   db.query(
+  //             "INSERT INTO articles (author, title, body, topic ) VALUES ($1,$2,$3,$4) RETURNING *;",
+  //             [author, title, body, topic]
+  //           );
 
-//   const res = await db
-//   .query(
-//     "select EXISTS (select * from users where username=$1 limit 1) as success;",
-//     [author]
-//   );
-
-//   if(res.rows){
-//     const nextRes = await db
-//   db.query(
-//             "INSERT INTO articles (author, title, body, topic ) VALUES ($1,$2,$3,$4) RETURNING *;",
-//             [author, title, body, topic]
-//           );
-
-//      return console.log("nextRes", nextRes);
-//       // return nextRes.rows;
-//   };
-// };
+  //      return console.log("nextRes", nextRes);
+  //       // return nextRes.rows;
+  //   };
+  // };
 
   const { author, title, body, topic } = newArticle;
 
@@ -148,6 +138,4 @@ exports.addArticle = async(newArticle) => {
       // console.log(result.rows[0]);
       return result && result.rows[0];
     });
-  }
-
-
+};
